640aa5233003452345b9c3ce69ca46c8d83d0ce9
Romain Crestey <rcrestey@gmail.com>
2025-04-07 09:42:55 +0200

    adjust hold on other key press

--- ZlWLp/keymap.c
+++ ZlWLp/keymap.c
@@ -214,7 +214,7 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
         case LT(1,KC_ENTER):
             // Immediately select the hold action when another key is pressed.
             return true;
-        case MT(MOD_LALT, KC_BSPC):
+        case MT(MOD_RALT, KC_BSPC):
             // Immediately select the hold action when another key is pressed.
             return true;
         default:
