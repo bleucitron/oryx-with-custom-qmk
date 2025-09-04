1c8d765916565a2668bda507831004fde2192327
Romain Crestey <rcrestey@gmail.com>
2025-09-04 15:38:02 +0200

    revert get_hold_on_other_key_press

--- waylq/keymap.c
+++ waylq/keymap.c
@@ -91,23 +91,21 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         // Whether or to immediately select the hold action when another key is pressed.
-        case MT(MOD_RALT, KC_SPACE):
-            return false;
-        // case MT(MOD_LCTL, KC_GRAVE):
-        //     return true;
-        // case MT(MOD_RCTL, KC_QUOTE):
-        //     return true;
-        // case LT(1, KC_SCLN):
-        //     return true;
-        // case MT(MOD_LGUI, KC_ESCAPE):
-        //     return true;
-        // case MT(MOD_RGUI, KC_ENTER):
-        //     return true;
-        // case MT(MOD_LALT, KC_BSPC):
-        //     return true;
+        case MT(MOD_LCTL, KC_GRAVE):
+            return true;
+        case MT(MOD_RCTL, KC_QUOTE):
+            return true;
+        case LT(1, KC_SCLN):
+            return true;
+        case MT(MOD_LGUI, KC_ESCAPE):
+            return true;
+        case MT(MOD_RGUI, KC_ENTER):
+            return true;
+        case MT(MOD_LALT, KC_BSPC):
+            return true;
 
         default:
-            return true;
+            return false;
     }
 }
 
