5add9afec7df3329293a1bad18e55e4b4cfe2248
Romain Crestey <rcrestey@gmail.com>
2025-10-23 14:10:08 +0200

    update get_hold_on_other_key_press

--- JRlY9/keymap.c
+++ JRlY9/keymap.c
@@ -397,7 +397,7 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
             return true;
         case MT(MOD_RALT, KC_ENTER):
             return true;
-        case LT(8, KC_BSPC):
+        case LT(2, KC_BSPC):
             return true;
 
         default:
