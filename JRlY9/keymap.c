747026a19562a17d5627ac64f89fd32bc2963972
Romain Crestey <rcrestey@gmail.com>
2025-12-10 15:23:57 +0100

    use layer 2 for numpad

--- JRlY9/keymap.c
+++ JRlY9/keymap.c
@@ -307,10 +307,8 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
         case MT(MOD_LGUI, KC_ESCAPE):
             return true;
         case MT(MOD_RALT, KC_ENTER):
-        // case MT(MOD_RALT, KC_BSPC):
             return true;
-        case LT(5, KC_BSPC):
-        // case LT(2, KC_ENTER):
+        case LT(2, KC_BSPC):
             return true;
 
         default:
