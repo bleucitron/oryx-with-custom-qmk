1f1e3b088b21a1aaea2ad536c24f1f5a748af4a9
Romain Crestey <rcrestey@gmail.com>
2026-03-26 13:46:43 +0100

    switch alt_gr and layer_2

--- JRlY9/keymap.c
+++ JRlY9/keymap.c
@@ -255,7 +255,11 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
             return true;
         case MT(MOD_RALT, KC_ENTER):
             return true;
+        case MT(MOD_RALT, KC_BSPC):
+            return true;
         case LT(2, KC_BSPC):
+            return true;
+        case LT(2, KC_ENTER):
             return true;
         case LT(2, KC_DOT):
             return true;
