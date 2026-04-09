81d3c9be9e763d14da3ca2275f487a4bee384900
Romain Crestey <rcrestey@gmail.com>
2026-04-09 17:59:22 +0200

    moves layer trigger on ENTER

--- JRlY9/keymap.c
+++ JRlY9/keymap.c
@@ -259,9 +259,7 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
             return true;
         case LT(2, KC_BSPC):
             return true;
-        case LT(2, KC_ENTER):
-            return true;
-        case LT(2, KC_DOT):
+        case LT(3, KC_ENTER):
             return true;
 
         default:
